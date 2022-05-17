c993718f503c05aa45252b0cdd3e5454a62f3289
Jovinus <lkh256@gmail.com>
2022-05-17 17:26:57 +0900

    bash file for execute python

--- model/train_model.sh
+++ model/train_model.sh
@@ -1,4 +1,1 @@
-python train.py --gpu_id 0 --aug_mode adasyn --max_epoch 400 --logdir binary_aging --class_type binary
-python train.py --gpu_id 0 --aug_mode randomover --max_epoch 400 --logdir binary_aging --class_type binary
-python train.py --gpu_id 0 --aug_mode randomunder --max_epoch 400 --logdir binary_aging --class_type binary
-python train.py --gpu_id 0 --aug_mode hybrid --max_epoch 400 --logdir binary_aging --class_type binary
+python train_multi.py --gpu_id 0 --aug_mode adasyn --max_epoch 200 --logdir quad_aging_all_multilabel --class_type quad_aging_all_multilabel
