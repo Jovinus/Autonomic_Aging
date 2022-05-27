52b2f430456033a8c3339cee539d23fbaf059353
KyungHyun Jovinus Lee <lkh256@gmail.com>
2022-05-27 15:57:24 +0900

    change options

--- model/train_model.sh
+++ model/train_model.sh
@@ -1,4 +1,4 @@
-python train.py --gpu_id 0 --aug_mode adasyn --max_epoch 400 --logdir binary_aging --class_type binary
-python train.py --gpu_id 0 --aug_mode randomover --max_epoch 400 --logdir binary_aging --class_type binary
-python train.py --gpu_id 0 --aug_mode randomunder --max_epoch 400 --logdir binary_aging --class_type binary
-python train.py --gpu_id 0 --aug_mode hybrid --max_epoch 400 --logdir binary_aging --class_type binary
+python train.py --gpu_id 0 --aug_mode adasyn --max_epoch 400 --logdir tri_aging_10fold --class_type tri_aging_10fold
+python train.py --gpu_id 0 --aug_mode randomover --max_epoch 400 --logdir tri_aging_10fold --class_type tri_aging_10fold
+python train.py --gpu_id 0 --aug_mode randomunder --max_epoch 400 --logdir tri_aging_10fold --class_type tri_aging_10fold
+python train.py --gpu_id 0 --aug_mode hybrid --max_epoch 400 --logdir tri_aging_10fold --class_type tri_aging_10fold
