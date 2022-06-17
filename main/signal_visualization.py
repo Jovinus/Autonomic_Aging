d4e5505ab5400e89d8b32554627805c9d37e6b54
KyungHyun Jovinus Lee <lkh256@gmail.com>
2022-06-17 15:02:54 +0900

    signal visualization code for test

--- main/signal_visualization.py
+++ main/signal_visualization.py
@@ -6,7 +6,7 @@ from ecgdetectors import Detectors
 from biosppy.signals import ecg
 import neurokit2 as nk
 # %%
-with open("../output/data/subject_1.json") as json_file:
+with open("../output/dataset/rri_hrv_data/subject_3_0.json") as json_file:
     ecg_json = json.load(json_file)
 
 # %%
@@ -65,15 +65,42 @@ test = nk.ecg_process(ecg_json['ECG1'], sampling_rate=1000)
 fig, ax = plt.subplots(1, 1, figsize=(20,10), facecolor='white')
 ax.plot(np.arange(0, 60*5*1000, 250) / 1000, np.interp(np.arange(0, 60*5*1000, 250), xp=test[1]['ECG_R_Peaks'][1:], fp=np.diff(test[1]['ECG_R_Peaks'])))
 plt.grid()
-plt.ylabel("mV", fontsize=14)
+plt.ylabel("RRI", fontsize=14)
 plt.xlabel("Times (sec)", fontsize=14)
 plt.savefig("../figure/rri.png", dpi=500)
 plt.show()
 # %%
 
-# %%
-signals, info = nk.ecg_process(ecg_json['ECG1'][0:1000*5], sampling_rate=1000)
+plt.rcParams['figure.figsize'] = [15, 9] 
 
-fig, ax = plt.subplots(1, 1, figsize=(10, 10))
-nk.ecg_plot(signals.head(5000), sampling_rate=1000)
+
+with open("../output/data/subject_3.json") as json_file:
+    ecg_json = json.load(json_file)
+
+# %%
+signals, info = nk.ecg_process(ecg_json['ECG1'][0:1000*60*2], sampling_rate=1000)
+
+nk.ecg_plot(signals, sampling_rate=1000)
+# %%
+r_peaks = signals["ECG_Clean"][signals['ECG_R_Peaks'].astype(bool)]
+fig, ax = plt.subplots(1, 1, figsize=(20,10), facecolor='white')
+ax.plot(signals['ECG_Clean'])
+ax.scatter(r_peaks.index, r_peaks, c='r')
+plt.grid()
+plt.ylabel("mV", fontsize=14)
+plt.xlabel("Times (sec)", fontsize=14)
+plt.show()
+
+
+# %%
+with open("../output/dataset/rri_hrv_data/subject_3_0.json") as json_file:
+    ecg_json = json.load(json_file)
+
+# %%
+fig, ax = plt.subplots(1, 1, figsize=(20,10), facecolor='white')
+ax.plot(ecg_json['RRI'])
+plt.grid()
+plt.ylabel("RRI", fontsize=14)
+plt.xlabel("Times (sec)", fontsize=14)
+plt.show()
 # %%
