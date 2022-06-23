adf85c24611af118102d3a88d587b9f011a37ebc
Jovinus <lkh256@gmail.com>
2022-06-23 15:40:01 +0900

    bash for hrv training

---
+++ model/train_model_1_hrv.sh
@@ -0,0 +1,2 @@
+python train_multi_hrv.py --gpu_id 1 --aug_mode randomover --max_epoch 500 --logdir quad_aging_all_multilabel_loss_weighted_hrv_1 --class_type quad_aging_all_multilabel_loss_weighted_hrv_1
+python train_multi_hrv.py --gpu_id 1 --aug_mode randomunder --max_epoch 500 --logdir quad_aging_all_multilabel_loss_weighted_hrv_1 --class_type quad_aging_all_multilabel_loss_weighted_hrv_1
