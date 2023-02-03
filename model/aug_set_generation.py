52511b8138cb34e8a3117e51f289539fca171b8f
KyungHyun Jovinus Lee <lkh256@gmail.com>
2023-02-03 14:53:07 +0900

    update path

--- model/aug_set_generation.py
+++ model/aug_set_generation.py
@@ -39,7 +39,7 @@ label_mapper = {1:0, 5:1, 6:1, 7:1} # Binary (2, 6-8)
 label_mapper = {0:0, 1:0, 2:0, 3:1, 4:1, 5:2, 6:2, 7:3, 
                 8:3, 9:3, 10:3, 11:3, 12:3, 13:3, 14:3}
 
-DATAPATH = "../output/dataset/rri_hrv_data"
+DATAPATH = "../output/dataset/rri_hrv_data_no"
 
 def get_rri(file_nm: str, data_dir_path: str) -> np.ndarray:
     """
