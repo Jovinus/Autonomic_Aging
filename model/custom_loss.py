776982bd34c6f8a2413e20085f8b2d75fc0807d3
KyungHyun Jovinus Lee <lkh256@gmail.com>
2023-02-04 07:59:17 +0900

    update-can align number of class automaticly

--- model/custom_loss.py
+++ model/custom_loss.py
@@ -24,20 +24,24 @@ class Cosine_Loss(pl.LightningModule):
         return loss
 
 class Multi_Loss(pl.LightningModule):
-    def __init__(self) -> None:
+    def __init__(self, num_class) -> None:
         super().__init__()
         # self.cross_entropy = nn.CrossEntropyLoss(weight=torch.Tensor([0.37, 0.86, 0.87, 0.9]))
         self.cross_entropy = nn.CrossEntropyLoss()
         self.cosine_similarity = nn.CosineEmbeddingLoss(reduction="mean")
         self.condition = torch.Tensor([1])
         self.mse_loss = nn.MSELoss()
+        self.num_class = num_class
 
     def forward(self, outputs, labels):
-        pred_proba, pred_reg = outputs[:, 0:4], outputs[:, 4]
+        pred_proba, pred_reg = outputs[:, 0:self.num_class], outputs[:, self.num_class]
         label_class, label_reg = labels[:, 0], labels[:, 1].type_as(pred_reg)
         loss_cross = self.cross_entropy(pred_proba, label_class)
-        loss_cosine = self.cosine_similarity(pred_proba, F.one_hot(label_class, num_classes=4), 
-                                             self.condition.type_as(loss_cross))
+        loss_cosine = self.cosine_similarity(
+            pred_proba, 
+            F.one_hot(label_class, num_classes=self.num_class), 
+            self.condition.type_as(loss_cross)
+        )
         loss_mse = self.mse_loss(pred_reg, label_reg) ## Numerical Value
         loss = loss_cosine + 0.1 * loss_cross + 1.0 * loss_mse
         return loss
