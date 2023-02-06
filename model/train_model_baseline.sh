0cb09b2bc9cceb63811bd4ab25aacf4db528ed60
KyungHyun Jovinus Lee <lkh256@gmail.com>
2023-02-06 18:44:57 +0900

    experiments setting

--- model/train_model_baseline.sh
+++ model/train_model_baseline.sh
@@ -1,14 +1,59 @@
-python train_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 500 --logdir quad_hrv_no --class_type quad_hrv_no
-python train_hrv.py --gpu_id 0 --aug_mode adasyn --max_epoch 500 --logdir quad_hrv_no --class_type quad_hrv_no
-python train_multi_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 500 --logdir quad_multi_hrv_no --class_type quad_multi_hrv_no
-python train_multi_hrv.py --gpu_id 0 --aug_mode adasyn --max_epoch 500 --logdir quad_multi_hrv_no --class_type quad_multi_hrv_no
+python train_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_hrv_no --class_type quad_hrv_no --dataset rri_hrv_data_osw_75
+python train_hrv.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_hrv_no --class_type quad_hrv_no --dataset rri_hrv_data_osw_75
+python train_multi_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_multi_hrv_no --class_type quad_multi_hrv_no --dataset rri_hrv_data_osw_75
+python train_multi_hrv.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_multi_hrv_no --class_type quad_multi_hrv_no --dataset rri_hrv_data_osw_75
 
-python train_rri.py --gpu_id 0 --aug_mode naive --max_epoch 500 --logdir quad_rri_no --class_type quad_rri_no
-python train_rri.py --gpu_id 0 --aug_mode adasyn --max_epoch 500 --logdir quad_rri_no --class_type quad_rri_no
-python train_multi_rri.py --gpu_id 0 --aug_mode naive --max_epoch 500 --logdir quad_multi_rri_no --class_type quad_multi_rri_no
-python train_multi_rri.py --gpu_id 0 --aug_mode adasyn --max_epoch 500 --logdir quad_multi_rri_no --class_type quad_multi_rri_no
+python train_rri.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_rri_no --class_type quad_rri_no --dataset rri_hrv_data_osw_75
+python train_rri.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_rri_no --class_type quad_rri_no --dataset rri_hrv_data_osw_75
+python train_multi_rri.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_multi_rri_no --class_type quad_multi_rri_no --dataset rri_hrv_data_osw_75
+python train_multi_rri.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_multi_rri_no --class_type quad_multi_rri_no --dataset rri_hrv_data_osw_75
 
-python train_rri_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 500 --logdir quad_rri_hrv_no --class_type quad_rri_hrv_no
-python train_rri_hrv.py --gpu_id 0 --aug_mode adasyn --max_epoch 500 --logdir quad_rri_hrv_no --class_type quad_rri_hrv_no
-python train_multi_rri_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 500 --logdir quad_multi_rri_hrv_no --class_type quad_multi_rri_hrv_no
-python train_multi_rri_hrv.py --gpu_id 0 --aug_mode adasyn --max_epoch 500 --logdir quad_multi_rri_hrv_no --class_type quad_multi_rri_hrv_no
+python train_rri_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_rri_hrv_no --class_type quad_rri_hrv_no --dataset rri_hrv_data_osw_75
+python train_rri_hrv.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_rri_hrv_no --class_type quad_rri_hrv_no --dataset rri_hrv_data_osw_75
+python train_multi_rri_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_multi_rri_hrv_no --class_type quad_multi_rri_hrv_no --dataset rri_hrv_data_osw_75
+python train_multi_rri_hrv.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_multi_rri_hrv_no --class_type quad_multi_rri_hrv_no --dataset rri_hrv_data_osw_75
+
+python train_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_hrv_no --class_type quad_hrv_no --dataset rri_hrv_data_osw_150
+python train_hrv.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_hrv_no --class_type quad_hrv_no --dataset rri_hrv_data_osw_150
+python train_multi_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_multi_hrv_no --class_type quad_multi_hrv_no --dataset rri_hrv_data_osw_150
+python train_multi_hrv.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_multi_hrv_no --class_type quad_multi_hrv_no --dataset rri_hrv_data_osw_150
+
+python train_rri.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_rri_no --class_type quad_rri_no --dataset rri_hrv_data_osw_150
+python train_rri.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_rri_no --class_type quad_rri_no --dataset rri_hrv_data_osw_150
+python train_multi_rri.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_multi_rri_no --class_type quad_multi_rri_no --dataset rri_hrv_data_osw_150
+python train_multi_rri.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_multi_rri_no --class_type quad_multi_rri_no --dataset rri_hrv_data_osw_150
+
+python train_rri_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_rri_hrv_no --class_type quad_rri_hrv_no --dataset rri_hrv_data_osw_150
+python train_rri_hrv.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_rri_hrv_no --class_type quad_rri_hrv_no --dataset rri_hrv_data_osw_150
+python train_multi_rri_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_multi_rri_hrv_no --class_type quad_multi_rri_hrv_no --dataset rri_hrv_data_osw_150
+python train_multi_rri_hrv.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_multi_rri_hrv_no --class_type quad_multi_rri_hrv_no --dataset rri_hrv_data_osw_150
+
+python train_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_hrv_no --class_type quad_hrv_no --dataset rri_hrv_data_osw_225
+python train_hrv.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_hrv_no --class_type quad_hrv_no --dataset rri_hrv_data_osw_225
+python train_multi_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_multi_hrv_no --class_type quad_multi_hrv_no --dataset rri_hrv_data_osw_225
+python train_multi_hrv.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_multi_hrv_no --class_type quad_multi_hrv_no --dataset rri_hrv_data_osw_225
+
+python train_rri.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_rri_no --class_type quad_rri_no --dataset rri_hrv_data_osw_225
+python train_rri.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_rri_no --class_type quad_rri_no --dataset rri_hrv_data_osw_225
+python train_multi_rri.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_multi_rri_no --class_type quad_multi_rri_no --dataset rri_hrv_data_osw_225
+python train_multi_rri.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_multi_rri_no --class_type quad_multi_rri_no --dataset rri_hrv_data_osw_225
+
+python train_rri_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_rri_hrv_no --class_type quad_rri_hrv_no --dataset rri_hrv_data_osw_225
+python train_rri_hrv.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_rri_hrv_no --class_type quad_rri_hrv_no --dataset rri_hrv_data_osw_225
+python train_multi_rri_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_multi_rri_hrv_no --class_type quad_multi_rri_hrv_no --dataset rri_hrv_data_osw_225
+python train_multi_rri_hrv.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_multi_rri_hrv_no --class_type quad_multi_rri_hrv_no --dataset rri_hrv_data_osw_225
+
+python train_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_hrv_no --class_type quad_hrv_no --dataset rri_hrv_data_osw_300
+python train_hrv.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_hrv_no --class_type quad_hrv_no --dataset rri_hrv_data_osw_300
+python train_multi_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_multi_hrv_no --class_type quad_multi_hrv_no --dataset rri_hrv_data_osw_300
+python train_multi_hrv.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_multi_hrv_no --class_type quad_multi_hrv_no --dataset rri_hrv_data_osw_300
+
+python train_rri.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_rri_no --class_type quad_rri_no --dataset rri_hrv_data_osw_300
+python train_rri.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_rri_no --class_type quad_rri_no --dataset rri_hrv_data_osw_300
+python train_multi_rri.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_multi_rri_no --class_type quad_multi_rri_no --dataset rri_hrv_data_osw_300
+python train_multi_rri.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_multi_rri_no --class_type quad_multi_rri_no --dataset rri_hrv_data_osw_300
+
+python train_rri_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_rri_hrv_no --class_type quad_rri_hrv_no --dataset rri_hrv_data_osw_300
+python train_rri_hrv.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_rri_hrv_no --class_type quad_rri_hrv_no --dataset rri_hrv_data_osw_300
+python train_multi_rri_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 1 --logdir quad_multi_rri_hrv_no --class_type quad_multi_rri_hrv_no --dataset rri_hrv_data_osw_300
+python train_multi_rri_hrv.py --gpu_id 0 --aug_mode adasyn --max_epoch 1 --logdir quad_multi_rri_hrv_no --class_type quad_multi_rri_hrv_no --dataset rri_hrv_data_osw_300
--- model/train_model_baseline_INU_1.sh
+++ model/train_model_baseline_INU_1.sh
@@ -1,3 +1,15 @@
-python train_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 500 --logdir quad_hrv_no --class_type quad_hrv_no
-python train_rri.py --gpu_id 0 --aug_mode adasyn --max_epoch 500 --logdir quad_rri_no --class_type quad_rri_no
-python train_rri_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 500 --logdir quad_rri_hrv_no --class_type quad_rri_hrv_no
+python train_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 500 --logdir quad_hrv_no --class_type quad_hrv_no --dataset rri_hrv_data_osw_75
+python train_rri.py --gpu_id 0 --aug_mode adasyn --max_epoch 500 --logdir quad_rri_no --class_type quad_rri_no --dataset rri_hrv_data_osw_75
+python train_rri_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 500 --logdir quad_rri_hrv_no --class_type quad_rri_hrv_no --dataset rri_hrv_data_osw_75
+
+python train_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 500 --logdir quad_hrv_no --class_type quad_hrv_no --dataset rri_hrv_data_osw_150
+python train_rri.py --gpu_id 0 --aug_mode adasyn --max_epoch 500 --logdir quad_rri_no --class_type quad_rri_no --dataset rri_hrv_data_osw_150
+python train_rri_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 500 --logdir quad_rri_hrv_no --class_type quad_rri_hrv_no --dataset rri_hrv_data_osw_150
+
+python train_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 500 --logdir quad_hrv_no --class_type quad_hrv_no --dataset rri_hrv_data_osw_225
+python train_rri.py --gpu_id 0 --aug_mode adasyn --max_epoch 500 --logdir quad_rri_no --class_type quad_rri_no --dataset rri_hrv_data_osw_225
+python train_rri_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 500 --logdir quad_rri_hrv_no --class_type quad_rri_hrv_no --dataset rri_hrv_data_osw_225
+
+python train_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 500 --logdir quad_hrv_no --class_type quad_hrv_no --dataset rri_hrv_data_osw_300
+python train_rri.py --gpu_id 0 --aug_mode adasyn --max_epoch 500 --logdir quad_rri_no --class_type quad_rri_no --dataset rri_hrv_data_osw_300
+python train_rri_hrv.py --gpu_id 0 --aug_mode naive --max_epoch 500 --logdir quad_rri_hrv_no --class_type quad_rri_hrv_no --dataset rri_hrv_data_osw_300
